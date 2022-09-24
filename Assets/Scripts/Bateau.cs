1635de06f30c4d75eaec0e64af546d462431ce03
Flupi <flupiiipi@gmail.com>
2022-09-24 05:31:39 +0200

    Le bateau se déplace proprement

--- Assets/Scripts/Bateau.cs
+++ Assets/Scripts/Bateau.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class Bateau : MonoBehaviour
 {
+    //Components
     private Rigidbody2D _rb;
 
+    //Kick
+    [Header("Kick")]
     [SerializeField] [Tooltip("A définir")] private float _kickSpeed = 10f;
     [SerializeField] private float _kickDuration;
-    [SerializeField] private float _kickLeftTime;
+    private float _kickLeftTime;
+    private float _kickPercentage;
 
-    [SerializeField] private float speed = 0f;
-    private Vector3 mousePosition;
+    //Movement
+    [Header("Speed")]
+    [SerializeField] [Tooltip("A définir")] private float _maxRotationSpeed;
+    private float speed = 0f;
+
+    //Target
     private Vector3 target;
+    private float maxDistanceFromTarget = 0.05f;
 
-    [SerializeField] [Tooltip("A définir")] private float _rotationSpeed;
 
     private void Start()
     {
@@ -21,23 +29,32 @@ public class Bateau : MonoBehaviour
 
     private void Update()
     {
+        //update target pos
         target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z * -1));
 
-        //Debug.Log("input " + Input.mousePosition + ", après transfo " + target);
+        //stop boat if near enough
+        if(Vector3.Distance(transform.position, target) <= maxDistanceFromTarget)
+        {
+            _kickLeftTime = 0f;
+        }
 
+        //update kick%
         float time = Time.deltaTime;
         _kickLeftTime -= time;
+        _kickPercentage = _kickLeftTime / _kickDuration;
 
-        if (_kickLeftTime > 0f)
+        //update speed according to kick%
+        if (_kickPercentage > 0f)
         {
-            speed = _kickSpeed * time * 100 * (_kickLeftTime / _kickDuration);
+            speed = _kickSpeed * time * 100 * _kickPercentage;
         }
-        else if (_kickLeftTime < 0f)
+        else if (_kickPercentage < 0f)
         {
             _kickLeftTime = 0f;
             speed = 0f;
         }
 
+        //trigger kick boost
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _kickLeftTime = _kickDuration;
@@ -48,35 +65,18 @@ public class Bateau : MonoBehaviour
     private void FixedUpdate()
     {
         _rb.velocity = transform.up * speed;
-
-        Quaternion quat = Quaternion.LookRotation(Vector3.forward, target - transform.position);
-
-        //quat.
-
-        _rb.MoveRotation(quat);
-
-        //LookTowards();
-        //_rb.Quaternion.Angle()
-        //_rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed));
+        LookTowards();
     }
 
     private void LookTowards()
     {
-        Debug.Log(Vector3.Angle(Vector3.up,target));
+        //Compute rotation needed to reach target
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, target - transform.position);   
+        
+        //Nerf rotation using maxRotationSpeed (how much if max kick) and kick (energy left from last speedup)
+        rotation = Quaternion.RotateTowards(transform.rotation, rotation, _maxRotationSpeed * _kickPercentage);
 
-        float accuracy = 5f;
-        float currentAngle = Vector3.Angle(transform.up, target - transform.position);
-
-        float wantedAngle = Vector3.Angle(Vector3.up, target - transform.position);
-
-        if(Mathf.Abs(currentAngle-wantedAngle) >= accuracy)
-        {
-            float plus, minus;
-
-            plus = currentAngle + _rotationSpeed;
-            minus = currentAngle - _rotationSpeed;
-
-            _rb.rotation = Mathf.Min(Mathf.Abs(plus-wantedAngle), Mathf.Abs(minus-wantedAngle));
-        }
+        //Apply nerfed rotation
+        _rb.SetRotation(rotation);                                                                              
     }
 }
